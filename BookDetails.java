1a346dbb7e59e4d7fe67880b4f7648ff5ce43345
Abhishek <ak1824871@gmail.com>
2025-01-30 18:28:45 +0530

    Java Class and Objects

---
+++ BookDetails.java
@@ -0,0 +1,24 @@
+class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
+
+public class BookDetails {
+    public static void main(String[] args) {
+        Book book = new Book("Java Programming", "James Gosling", 499.99);
+        book.displayDetails();
+    }
+}
